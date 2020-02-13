e625c951c1b528d86a8600d41de097d7c78b6e45
Saizen408 <llkoole7@gmail.com>
2020-02-12 21:51:03 -0800

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp()); //'runApp' ensures the widget tree gets created
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+      'sls',
+    ];
+    //'build' returns widget tree which tells flutter to draw onto the screen
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () {
+                _answerQuestion();
+                print('Answer 1 chosen!');
+              },
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () {
+                _answerQuestion();
+                print('Answer 2 chosen!');
+              },
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                _answerQuestion();
+                print('Answer 3 chosen!');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
